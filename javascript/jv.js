21551b722ee14559cba341a24a9623f9e3f401b2
FaithVD <r0980271@student.vives.be>
2025-12-17 14:11:09 +0100

    commit 2

---
+++ javascript/jv.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function ChangeText(){
+    document.getElementById("message").innerHTML = "The JavaScript is working!";
+
+
+    document.getElementById("message").style.color = "red";
+
+    alert("You clicked the button!");
+}
+
